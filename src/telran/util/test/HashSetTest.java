0c48ec395e596c6dfc21c9ae7aff3d3c1ee3572c
Yuragranovsky <yuragranovsky@gmail.com>
2024-05-28 12:38:42 +0300

    CW #24 adds comments inside HashSetTest class

--- src/telran/util/test/HashSetTest.java
+++ src/telran/util/test/HashSetTest.java
@@ -12,11 +12,13 @@ public class HashSetTest extends SetTest {
 @Override
 @BeforeEach
 void setUp() {
-	collection = new HashSet<>(3, 0.75f);
+	collection = new HashSet<>(3, 0.75f); //for testing Hash Table re-creation
 	super.setUp();
 }
 @Override
 protected void runTest(Integer[] expected) {
+	//since iterating of HashSet is done at not defined order
+	//actual and expected arrays should be sorted
 	Integer[]actual = collection.stream().sorted().toArray(Integer[]::new);
 	Arrays.sort(expected);
 	assertArrayEquals(expected, actual);
