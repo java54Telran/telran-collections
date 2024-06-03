0f017d7df0118e6443aa26ebd9a0f08446081c7b
Yuragranovsky <yuragranovsky@gmail.com>
2024-06-03 22:15:30 +0300

    CW #25 with TODO's

--- src/telran/util/TreeSet.java
+++ src/telran/util/TreeSet.java
@@ -1,55 +1,190 @@
 package telran.util;
 
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class TreeSet<T> implements SortedSet<T> {
+private static class Node<T> {
+	T data;
+	Node<T> parent;
+	Node<T> left;
+	Node<T> right;
+	Node(T data) {
+		this.data = data;
+	}
+	
+}
+private class TreeSetIterator implements Iterator<T> {
+	Node<T> current = getLeastFrom(root);
+	@Override
+	public boolean hasNext() {
+		
+		return current != null;
+	}
 
 	@Override
+	public T next() {
+		if(!hasNext()) {
+			throw new NoSuchElementException();
+		}
+		T res = current.data;
+		current = getCurrent(current);
+		return res;
+	}
+	
+}
+	Node<T> root;
+	int size;
+	private Comparator<T> comp;
+	public TreeSet(Comparator<T> comp) {
+		this.comp = comp;
+	}
+	@SuppressWarnings("unchecked")
+	public TreeSet() {
+		this((Comparator<T>)Comparator.naturalOrder());
+	}
+	@Override
 	public T get(T pattern) {
+		Node<T> node = getNode(pattern);
+		
+		return node == null ? null : node.data;
+	}
+
+	private Node<T> getNode(T pattern) {
+		Node<T> res = null;
+		Node<T> node = getParentOrNode(pattern);
+		if (node != null && comp.compare(node.data, pattern) == 0) {
+			res = node;
+		}
+		return res;
+	}
+
+	private Node<T> getParentOrNode(T pattern) {
+		Node<T> current = root;
+		Node<T> parent = null;
+		int compRes = 0;
+		while(current != null && (compRes = comp.compare(pattern, current.data)) != 0) {
+			parent = current;
+			current = compRes > 0 ? current.right : current.left;
+		}
+		return current == null ? parent : current;
+	}
+
+	public Node<T> getCurrent(Node<T> current) {
+		// TODO Auto-generated method stub
+		//Algorithm see on the board
+		return current.right != null ? getLeastFrom(current.right) :
+			getFirstGreaterParent(current);
+	}
+
+	private Node<T> getFirstGreaterParent(Node<T> current) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	private Node<T> getLeastFrom(Node<T> node) {
+		if (node != null) {
+			
+			while(node.left != null) {
+				node = node.left;
+			}
+		}
+		return node;
 	}
 
 	@Override
 	public boolean add(T obj) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+		if(obj == null) {
+			throw new NullPointerException();
+		}
+		if(!contains(obj)) {
+			res = true;
+			Node<T> node = new Node<>(obj);
+			if(root == null) {
+				addRoot(node);
+			} else {
+				addWithParent(node);
+			}
+			size++;
+		}
+		return res;
+	}
+
+	private void addWithParent(Node<T> node) {
+		Node<T> parent = getParent(node);
+		if(comp.compare(node.data,parent.data) > 0) {
+			parent.right = node;
+		} else {
+			parent.left = node;
+		}
+		node.parent = parent;
+		
+	}
+
+	private Node<T> getParent(Node<T> node) {
+		Node<T> parent = getParentOrNode(node.data);
+		return parent;
+	}
+
+	private void addRoot(Node<T> node) {
+		root = node;
+		
 	}
 
 	@Override
 	public boolean remove(T pattern) {
+		boolean res = false;
+		Node<T> node = getNode(pattern);
+		if (node != null) {
+			removeNode(node);
+			res = true;
+		}
+		return res;
+	}
+
+	private void removeNode(Node<T> node) {
 		// TODO Auto-generated method stub
-		return false;
+		
 	}
 
 	@Override
 	public boolean contains(T pattern) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return getNode(pattern) != null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return size;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new TreeSetIterator();
 	}
 
 	@Override
 	public T first() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return root == null ? null : getLeastFrom(root).data;
 	}
 
 	@Override
 	public T last() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return root == null ? null : getGreatesFrom(root).data;
+	}
+
+	private Node<T> getGreatesFrom(Node<T> node) {
+		if(node != null) {
+			while(node.right != null) {
+				node = node.right;
+			}
+		}
+		return node;
 	}
 
 }
