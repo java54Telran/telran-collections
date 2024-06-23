5e47c3bc663e3d947740d5eee1a2096723b238b3
Yuragranovsky <yuragranovsky@gmail.com>
2024-06-23 16:44:47 +0300

    Completes TreeMap

--- src/telran/util/TreeMap.java
+++ src/telran/util/TreeMap.java
@@ -4,28 +4,29 @@ import java.util.Comparator;
 
 public class TreeMap<K, V> extends AbstractMap<K, V> implements SortedMap<K, V> {
 
+	TreeSet<Entry<K, V>> treeSet = ((TreeSet<Entry<K, V>>)set);
 	@Override
 	public K firstKey() {
-		// TODO Auto-generated method stub
-		return null;
+		Entry<K, V> entry = treeSet.first();
+		return entry.getKey();
 	}
 
 	@Override
 	public K lastKey() {
-		// TODO Auto-generated method stub
-		return null;
+		Entry<K, V> entry = treeSet.last();
+		return entry.getKey();
 	}
 
 	@Override
 	public K floorKey(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		Entry<K, V> entry = treeSet.floor(new Entry<>(key, null));
+		return entry == null ? null : entry.getKey();
 	}
 
 	@Override
 	public K ceilingKey(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		Entry<K, V> entry = treeSet.ceiling(new Entry<>(key, null));
+		return entry == null ? null : entry.getKey();
 	}
 
 	@Override
@@ -34,10 +35,12 @@ public class TreeMap<K, V> extends AbstractMap<K, V> implements SortedMap<K, V> 
 		return new TreeSet<K>();
 	}
 	public TreeMap() {
-		set = new TreeSet<>();
+		this(Comparator.naturalOrder());
+		
 	}
 	public TreeMap(Comparator<Entry<K, V>> comp) {
 		set = new TreeSet<>(comp);
+		treeSet = ((TreeSet<Entry<K, V>>)set);
 	}
 
 }
